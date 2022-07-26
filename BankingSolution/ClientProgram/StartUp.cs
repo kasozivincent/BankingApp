4942dd0dec275995917a053a87528ab66b5c445f
Kasozi Vincent <kasozivincentug507@gmail.com>
2022-07-27 08:09:32 +1200

    Program entry point

---
+++ BankingSolution/ClientProgram/StartUp.cs
@@ -0,0 +1,11 @@
+namespace ClientProgram
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            BankProgram program = new BankProgram();
+            program.Run();
+        }
+    }
+}
