c8b52546156ef2375fae6024481cd39275b46da1
Kasozi Vincent <kasozivincentug507@gmail.com>
2022-07-27 10:21:58 +1200

    Composition root

--- BankingSolution/ClientProgram/StartUp.cs
+++ BankingSolution/ClientProgram/StartUp.cs
@@ -1,10 +1,15 @@
+using Domain;
+
 namespace ClientProgram
 {
     public class StartUp
     {
         public static void Main()
         {
-            BankClient program = new ();
+            int accountCounter = 1;
+            IDictionary<int, BankAccount> accounts =  new Dictionary<int, BankAccount>();
+            Bank bank = new (accounts, accountCounter);
+            BankClient program = new (bank);
             program.Run();
         }
     }
