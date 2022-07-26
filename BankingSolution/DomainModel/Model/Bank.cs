181f96e5c44541a32ac50cf5d168911a2c6e4175
Kasozi Vincent <kasozivincentug507@gmail.com>
2022-07-27 08:23:45 +1200

    createNewAccount implementation

--- BankingSolution/DomainModel/Model/Bank.cs
+++ BankingSolution/DomainModel/Model/Bank.cs
@@ -2,6 +2,17 @@ namespace Domain
 {
     public class Bank
     {
+        private Dictionary<int, int> _accounts = new Dictionary<int, int>();
+        private double _rate = 0.01;
+        private int _accountNumber = 0;
+
+        public int CreateNewAccount()
+        {
+            _accountNumber++;
+            _accounts.Add(_accountNumber, 0);
+            return _accountNumber;
+        }
+
         
     }
 }
