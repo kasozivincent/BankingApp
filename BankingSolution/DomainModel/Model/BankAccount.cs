8140915825e4792bd0e0546374272d3048d9a4b0
Kasozi Vincent <kasozivincentug507@gmail.com>
2022-07-27 10:00:00 +1200

    refactoring bankaccount

--- BankingSolution/DomainModel/Model/BankAccount.cs
+++ BankingSolution/DomainModel/Model/BankAccount.cs
@@ -10,8 +10,22 @@ namespace Domain
         public int AccountNumber { get; init; }
         public int Balance { get; set; }
         public Status Status { get; set; }
-        
+
+        private double _rate = 0.01;
+
         public BankAccount(int accountNumber)
             => this.AccountNumber = accountNumber;
+
+        public void DepositMoney(int amount) 
+            => Balance += amount;
+
+        public bool hasEnoughCollateral(int loanAmount) 
+            => Balance >= loanAmount/2;
+
+        public override string ToString()
+            => $"Account Number: {AccountNumber} Account Balance: {Balance} Account Status: {Status}";
+
+        public void AddInterest() 
+            => Balance += (int) (Balance * _rate);
     }
 }
