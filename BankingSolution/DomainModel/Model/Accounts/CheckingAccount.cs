3a92c70ba3ea443cae7f88ea3529fde3a310fba6
Kasozi Vincent <kasozivincentug507@gmail.com>
2022-07-28 09:01:12 +1200

    Made CheckingAccount abstract

--- BankingSolution/DomainModel/Model/Accounts/CheckingAccount.cs
+++ BankingSolution/DomainModel/Model/Accounts/CheckingAccount.cs
@@ -1,19 +1,15 @@
 namespace Domain.Accounts
 {
-    public class CheckingAccount : BankAccount
+    public abstract class CheckingAccount : BankAccount
     {
         public CheckingAccount(int accountNumber) : base(accountNumber) {}
 
         public override void AddInterest()
         {}
 
-        public override object Clone()
-            => new CheckingAccount(this.AccountNumber){Balance = this.Balance, Status = this.Status};
 
         public override bool hasEnoughCollateral(int loanAmount)
             =>  Balance >= 2 * loanAmount / 3;
 
-        public override string ToString()
-            => $"(Checking Account) Account No: {AccountNumber} Balance: {Balance} Status: {Status}";
     }
 }
