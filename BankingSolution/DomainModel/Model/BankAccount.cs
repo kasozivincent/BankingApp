c7bffb1fa82c1b45f7a2609086d3b182c90f1e45
Kasozi Vincent <kasozivincentug507@gmail.com>
2022-07-28 07:55:20 +1200

    Savings and Checking Accounts

--- BankingSolution/DomainModel/Model/BankAccount.cs
+++ BankingSolution/DomainModel/Model/BankAccount.cs
@@ -8,24 +8,40 @@ namespace Domain
     public class BankAccount
     {
         public int AccountNumber { get; init; }
+
+        private int type;
+
         public int Balance { get; set; }
         public Status Status { get; set; }
-
         private double _rate = 0.01;
 
-        public BankAccount(int accountNumber)
-            => this.AccountNumber = accountNumber;
+        public BankAccount(int accountNumber, int type){
+            this.AccountNumber = accountNumber;
+            this.type = type;
+        }
+            
 
         public void DepositMoney(int amount) 
             => Balance += amount;
 
-        public bool hasEnoughCollateral(int loanAmount) 
-            => Balance >= loanAmount/2;
+        public bool hasEnoughCollateral(int loanAmount){
+            if(type == 1)
+                return Balance >= loanAmount/2;
+            else 
+                return Balance >= (2 * loanAmount)/3;
+        }
+            
 
-        public override string ToString()
-            => $"Account Number: {AccountNumber} Account Balance: {Balance} Account Status: {Status}";
+        public override string ToString(){
+            if(type == 1)
+                return $"Saving Account, Account Number: {AccountNumber} Account Balance: {Balance} Account Status: {Status}";
+            else 
+                return $"Checking Account, Account Number: {AccountNumber} Account Balance: {Balance} Account Status: {Status}";
+        }
 
-        public void AddInterest() 
-            => Balance += (int) (Balance * _rate);
+        public void AddInterest() {
+            if(type == 1)
+                Balance += (int) (Balance * _rate);
+        }
     }
 }
