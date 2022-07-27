1ac313963bf87f03198b2c9875753d734603b554
Kasozi Vincent <kasozivincentug507@gmail.com>
2022-07-28 08:10:30 +1200

    Made BankAccount abstract

--- BankingSolution/DomainModel/Model/BankAccount.cs
+++ BankingSolution/DomainModel/Model/BankAccount.cs
@@ -5,43 +5,25 @@ namespace Domain
         Domestic, Foreign
     }
 
-    public class BankAccount
+    public abstract class BankAccount
     {
         public int AccountNumber { get; init; }
-
-        private int type;
 
         public int Balance { get; set; }
         public Status Status { get; set; }
         private double _rate = 0.01;
 
-        public BankAccount(int accountNumber, int type){
-            this.AccountNumber = accountNumber;
-            this.type = type;
-        }
+        public BankAccount(int accountNumber)
+            => this.AccountNumber = accountNumber;
             
 
         public void DepositMoney(int amount) 
             => Balance += amount;
 
-        public bool hasEnoughCollateral(int loanAmount){
-            if(type == 1)
-                return Balance >= loanAmount/2;
-            else 
-                return Balance >= (2 * loanAmount)/3;
-        }
-            
 
-        public override string ToString(){
-            if(type == 1)
-                return $"Saving Account, Account Number: {AccountNumber} Account Balance: {Balance} Account Status: {Status}";
-            else 
-                return $"Checking Account, Account Number: {AccountNumber} Account Balance: {Balance} Account Status: {Status}";
-        }
+        public abstract bool hasEnoughCollateral(int loanAmount);
 
-        public void AddInterest() {
-            if(type == 1)
-                Balance += (int) (Balance * _rate);
-        }
+        public abstract override string ToString();
+        public abstract void AddInterest();
     }
 }
