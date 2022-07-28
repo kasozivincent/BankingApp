77aa5ef08051cb83155b29a9ed24a3ded1288aee
Kasozi Vincent <kasozivincentug507@gmail.com>
2022-07-29 08:25:44 +1200

    Fixed the constructor args

--- BankingSolution/DomainModel/Model/Accounts/InterestCheckingAccount.cs
+++ BankingSolution/DomainModel/Model/Accounts/InterestCheckingAccount.cs
@@ -1,12 +1,14 @@
+using Model;
+
 namespace Domain.Accounts
 {
     public class InterestCheckingAccount : CheckingAccount
     {
-        public InterestCheckingAccount(int accountNumber) : base(accountNumber){}
+        public InterestCheckingAccount(int accountNumber, IStatus status) : base(accountNumber, status){}
 
        
         public override object Clone()
-             => new InterestCheckingAccount(this.AccountNumber){Balance = this.Balance, Status = this.Status};
+             => new InterestCheckingAccount(this.AccountNumber, this.Status){Balance = this.Balance, Status = this.Status};
 
         protected override string GetAccountType()
             => "InterestingCheckingAccount";
