79039d5a473c06fb357ab72975fbd163fd54a6bb
Kasozi Vincent <kasozivincentug507@gmail.com>
2022-07-29 08:31:17 +1200

    Fixed the constructor args and clone method

--- BankingSolution/DomainModel/Model/Accounts/RegularCheckingAccount.cs
+++ BankingSolution/DomainModel/Model/Accounts/RegularCheckingAccount.cs
@@ -1,11 +1,13 @@
+using Model;
+
 namespace Domain.Accounts
 {
     public class RegularCheckingAccount : CheckingAccount
     {
-        public RegularCheckingAccount(int accountNumber) : base(accountNumber){}
+        public RegularCheckingAccount(int accountNumber, IStatus status) : base(accountNumber, status){}
 
         public override object Clone()
-            => new RegularCheckingAccount(this.AccountNumber){Balance = this.Balance, Status = this.Status};
+            => new RegularCheckingAccount(this.AccountNumber, this.Status){Balance = this.Balance, Status = this.Status};
 
         protected override string GetAccountType()
             => "RegularCheckingAccount";
