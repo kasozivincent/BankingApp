03403e8509a1a7e869fdd9af0b4ac531aae75455
Kasozi Vincent <kasozivincentug507@gmail.com>
2022-07-29 08:22:40 +1200

    Fixed the constructor args

--- BankingSolution/DomainModel/Model/Accounts/CheckingAccount.cs
+++ BankingSolution/DomainModel/Model/Accounts/CheckingAccount.cs
@@ -1,9 +1,10 @@
+using Model;
+
 namespace Domain.Accounts
 {
     public abstract class CheckingAccount : BankAccount
     {
-        public CheckingAccount(int accountNumber) : base(accountNumber) {}
-
+        public CheckingAccount(int accountNumber, IStatus status) : base(accountNumber, status) {}
 
         protected override double GetRatio()
             => 2.0 / 3.0;
